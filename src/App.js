8a122ec07440283be8043d381b855dd3e93046ec
Mohit <mohit167@Mohits-MacBook-Air.local>
2023-10-16 23:12:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Works from "./components/Works";
+import "./index.css"
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Works />
+      <Contact />
     </div>
   );
 }
-
 export default App;
